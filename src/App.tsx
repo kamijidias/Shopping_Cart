eac60a2d0737806fd4fe719f8a49014625e1f762
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-08 11:50:59 -0300

    Estruturando

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from 'react-bootstrap'
+
+function App() {
+  return <Container><h1>Hi</h1></Container> 
+}
+
+export default App
