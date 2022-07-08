0f6894f6111836eb6673e0a261a1b2f24336380c
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-08 14:34:31 -0300

    Criando a página Store

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ import { Navbar } from './components/Navbar'
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/store" element={<Store />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </Container>
-    </>
+    <div>
+    <Navbar />
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </div>
   )
 }
 
