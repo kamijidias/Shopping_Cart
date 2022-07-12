4707c8fff3cb3eb1fa69b6a6af08f2ff5d4d9616
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-07-12 14:43:43 -0300

    pequenas alterações

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -31,7 +31,7 @@ export function Navbar() {
                 transform: "translate(25%, 25%)",
               }}
             >
-                hi
+                3
             </div>
                 </Button>
             </Container>
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -20,11 +20,10 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                 <span className="fs-2">{name}</span>
-                <span className="ms-2 text-muted">{formatCurrency (price)}</span>
+                <span className="ms-2 text-muted">{formatCurrency ( price)}</span>
             </Card.Title>
         </Card.Body> 
         </Card>
     )
 }
 
-//24:51
